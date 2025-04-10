b6ade5a1cfa4c56bdbe7e2788b73ac779b67a872
Cristóvão GUIHO <88683894+CGuiho@users.noreply.github.com>
2025-04-10 15:17:08 +0000

    Fix import path for importSignal in indicator tests

--- indicator/indicator.test.py
+++ indicator/indicator.test.py
@@ -2,7 +2,7 @@
   @copyright Copyright © 2025 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved. 
 """ 
 
-from ..importer.importer import importSignal
+from importer.importer import importSignal
 from indicator import calculateIndicators
 
 signal = importSignal("./tp-equilibrator/data/1-roulement-sain/acc_00001.csv")
