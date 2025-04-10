9893cd1cc4c92c895d56fa66db5e2623209eb553
Cristóvão GUIHO <88683894+CGuiho@users.noreply.github.com>
2025-04-10 15:11:25 +0000

    Update tests to import calculateIndicators and print signal data

--- indicator/indicator.test.py
+++ indicator/indicator.test.py
@@ -2,8 +2,13 @@
   @copyright Copyright © 2025 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved. 
 """ 
 
-from importer import importSignal
+from ..importer.importer import importSignal
+from indicator import calculateIndicators
 
-data = importSignal("./tp-equilibrator/data/1-roulement-sain/acc_00001.csv")
-print(data)
+signal = importSignal("./tp-equilibrator/data/1-roulement-sain/acc_00001.csv")
+print(signal)
+
+indicators = calculateIndicators(signal)
+print(signal)
+
 
