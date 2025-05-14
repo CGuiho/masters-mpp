3b67b8cfcc9ea8a9e2e35a07adb77ce4dc703d01
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2025-05-14 18:41:29 +0200

    Add signal type definitions and copyright notice

---
+++ reducer/source/signal/signal.ts
@@ -0,0 +1,17 @@
+/**
+ * @copyright Copyright © 2025 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+export type { Matrix, Scalar, Signal, Tensor0D, Tensor1D, Tensor2D, Tensor3D, Tensor4D, Vector }
+
+type Scalar = number
+type Vector = number[]
+type Matrix = number[][]
+
+type Tensor0D = number
+type Tensor1D = number[]
+type Tensor2D = number[][]
+type Tensor3D = number[][][]
+type Tensor4D = number[][][][]
+
+type Signal = Vector
