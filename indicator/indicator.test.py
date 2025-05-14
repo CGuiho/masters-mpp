11495128ba8f14bebb8fdb146e6e2d6227d91725
Cristóvão GUIHO <88683894+CGuiho@users.noreply.github.com>
2025-05-14 06:27:47 +0000

    Fix import order in indicator test file

--- indicator/indicator.test.py
+++ indicator/indicator.test.py
@@ -2,8 +2,8 @@
   @copyright Copyright © 2025 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved. 
 """ 
 
-from importer.importer import importSignal
 from indicator import calculateIndicators
+from importer.importer import importSignal
 
 signal = importSignal("./tp-equilibrator/data/1-roulement-sain/acc_00001.csv")
 print(signal)
