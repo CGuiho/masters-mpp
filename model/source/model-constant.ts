9320678ad0e9360de8776005bd9c3311502d15b6
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2025-07-21 21:35:54 +0200

    INPUT_SIZE, OUTPUT_SIZE

---
+++ model/source/model-constant.ts
@@ -0,0 +1,15 @@
+/**
+ * @copyright Copyright © 2025 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+/**
+ * File name: model-constant.ts
+ * Relative file path: model\source\model-constant.ts
+ * The relative path is the file path on workspace or folder.
+ */
+
+export { INPUT_SIZE, OUTPUT_SIZE }
+export type { }
+
+const INPUT_SIZE = 4 // Number of input features
+const OUTPUT_SIZE = 4 // Number of output neurons
