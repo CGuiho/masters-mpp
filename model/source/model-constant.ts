2f000bc4849e6736756644bc9641138d8661e2c6
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2025-07-21 21:36:49 +0200

    HIDDEN_LAYER_SIZES

--- model/source/model-constant.ts
+++ model/source/model-constant.ts
@@ -8,8 +8,9 @@
  * The relative path is the file path on workspace or folder.
  */
 
-export { INPUT_SIZE, OUTPUT_SIZE }
+export { INPUT_SIZE, OUTPUT_SIZE, HIDDEN_LAYER_SIZES }
 export type { }
 
 const INPUT_SIZE = 4 // Number of input features
 const OUTPUT_SIZE = 4 // Number of output neurons
+const HIDDEN_LAYER_SIZES = [4, 4] // As per the requirement
